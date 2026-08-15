b2fbeb5be27187e0f79bc63790e344154e25fa83
Abdalrahman Aldabbas <aldabbas333abdalrahman@gmail.com>
2026-08-16 00:21:34 +0300

    feat: add Arabic/English internationalization with RTL support

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
+import { LanguageProvider } from "./i18n/LanguageContext";
 import "./main.css";
+
+const savedLang = localStorage.getItem("lang");
+if (savedLang === "en") {
+  document.documentElement.dir = "ltr";
+  document.documentElement.lang = "en";
+} else {
+  document.documentElement.dir = "rtl";
+  document.documentElement.lang = "ar";
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </StrictMode>
 );
